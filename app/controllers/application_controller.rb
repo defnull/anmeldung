1c370681675d02831959b146fcbefba03790dfb9
Johannes Hass <JohannesHass@googlemail.com>
2011-04-15 18:46:16 +0200

    added catch-all for 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
   
-  def set_locale
-  	I18n.locale = params[:locale]
-  end
+  private
   
-  def default_url_options(options={})
-    { :locale => I18n.locale }
-  end
+    def set_locale
+  	  I18n.locale = params[:locale]
+    end
+  
+    def default_url_options(options={})
+      { :locale => I18n.locale }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,60 +8,10 @@ Anmeldung::Application.routes.draw do
   match ':locale/hosts/create' => 'hosts#create', :as => :createhost
   match ':locale/users/confirm' => 'users#confirm', :as => :confirm
   match ':locale/' => 'users#index', :as => :index
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  
   root :to => "users#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  
+  # Catch-all to get those users who have weird start pages so they don't get
+  # a 404 when first connecting to our net
+  match '*path', :to => 'users#index'
 end
