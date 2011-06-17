f8b08a8b85ef0a848d5a14491ffbef0ff621cd2a
Johannes Hass <JohannesHass@googlemail.com>
2011-06-17 15:36:39 +0200

    Clarification for limited users with a limited IP

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,14 @@
 module UsersHelper
   def user_has_to_reboot
     if request.remote_ip.split(".")[2].to_i == 8
-      note = "<h1 class='reboot_note reboot_top'>"
+      note = "<div class='ui-state-error ui-corner-all'>"
+      note += "<h1>"
       note += t 'prelim.you_have_to_get_a_new_ip_adress'
-      note += "</h1><h2 class='reboot_note reboot_bottom'>"
+      note += "</h1><h2>"
       note += t 'prelim.either_reboot_or_unplug'
-      note += "</h2>"
+      note += "</h2><h2>"
+      note += t 'prelim.updates_all_ten_minutes'
+      note += "</h2></div>"
       note.html_safe
     end
   end
